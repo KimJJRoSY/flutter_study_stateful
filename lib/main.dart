52a4f8e5ce1c3ef661bfd3e43fea507aed391e9b
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-01-27 00:43:24 +0900

    전역 스타일링,Bulid context 사용법

--- lib/main.dart
+++ lib/main.dart
@@ -12,46 +12,48 @@ class App extends StatefulWidget {
 
 // 상태 있는 위젯
 class _AppState extends State<App> {
-  int counter = 0;
-
-  List<int> numbers = [];
-
-  void onClicked() {
-    // setState: State 클래스에서 데이터가 변경되었다고 알리는 함수
-    // 이 함수 없으면 build 메서드 다시 실행 X -> 상태 변화 생겨도 ui 변경 X
-    // 굳이 setState안에 실행 함수 안 적어도 되는데 가독성 때문에 적는거 추천
-
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Color(0xFFF4EDDB),
-      body: Center(
-        child: Column(
-          //
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Click Count',
-              style: TextStyle(
-                fontSize: 30,
-              ),
+        // 스타일링 전역 관리
+        theme: ThemeData(
+          textTheme: TextTheme(
+            titleLarge: TextStyle(
+              color: Colors.red,
             ),
-            for (var num in numbers) Text('$num'),
-            // flutter에서 제공하는 버튼
-            IconButton(
-              iconSize: 40,
-              onPressed: onClicked,
-              icon: Icon(Icons.add_box_rounded),
-            )
-          ],
+          ),
         ),
+        home: Scaffold(
+          backgroundColor: Color(0xFFF4EDDB),
+          body: Center(
+            child: Column(
+              //
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // 위젯화
+                MyLargeTitle(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  // contect는 Text 이전에 있는 모든 상위 요소들에 대한 정보 => 부모 요소 정보
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        //여기에서 theme까지 context로 거슬러 올라가기 가능
+        color: Theme.of(context).textTheme.titleLarge?.color, //? <- null 방지
       ),
-    ));
+    );
   }
 }
