bdc6d995b5563f2a5690cd33d334bc53969c7630
KimJeongJoo <kjjeaus@hufs.ac.kr>
2025-01-27 15:39:25 +0900

    widget lifecycle 알아보기

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 
 // 상태 있는 위젯
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,7 +39,10 @@ class _AppState extends State<App> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 // 위젯화
-                MyLargeTitle(),
+                // showTitle이 false일 때, nothing을 보여줌
+                showTitle ? MyLargeTitle() : const Text('nothing'),
+                IconButton(
+                    onPressed: toggleTitle, icon: Icon(Icons.remove_red_eye))
               ],
             ),
           ),
@@ -39,14 +50,36 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  // 상태 초기화를 위한 메서드
+  // 항상 build 메서드보다 먼저 호출되어야 함 + 한 번만 호출됨
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  // dispose는 위젯이 스크린에서 제거될 때 호출되는 메서드
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
   // contect는 Text 이전에 있는 모든 상위 요소들에 대한 정보 => 부모 요소 정보
   Widget build(BuildContext context) {
+    // build 메서드는 위젯에서 ui를 만듦
+    print('build');
     return Text(
       'My Large Title',
       style: TextStyle(
